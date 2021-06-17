2d491df40ecc84c770d126ca1b9a5a06a30569d0
s3718342 <s3718342@student.rmit.edu.au>
2021-06-17 10:52:35 +0000

    Created new migration to add fk to answer

--- db/migrate/20210617040934_create_answers.rb
+++ db/migrate/20210617040934_create_answers.rb
@@ -3,7 +3,6 @@ class CreateAnswers < ActiveRecord::Migration[5.2]
     create_table :answers do |t|
       t.string :description
       t.boolean :correct
-      t.belongs_to :question, index: true, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_041135) do
+ActiveRecord::Schema.define(version: 2021_06_17_104903) do
 
   create_table "answers", force: :cascade do |t|
     t.string "description"
     t.boolean "correct"
-    t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "question_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
